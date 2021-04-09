1d6115052ae039f253de992a84a4daad44a683bd
= <=>
2021-04-09 05:19:07 -0400

    Mod: 09/04/2021

---
+++ scripts/Estrategia_3_oxigeno/plasticSimulation_individualTestFLYCOP.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+############ FLYCOP ############
+# Author: Beatriz García-Jiménez
+# April 2018
+################################
+
+# Running an individual test, for a particular consortium configuration given by arguments
+#cp -p -R ecoliLongTerm_TemplateOptimizeConsortiumV0 ecoliLongTerm_TestXX
+#cd ecoliLongTerm_TestXX
+#python3 ../../Scripts/ecoliLongTerm_individualTestFLYCOP.py -10 -16 -11 -12 -6 -16 'Yield'
+
+import sys
+import importlib
+sys.path.append('../../Scripts')
+import plasticDegradationSimulationEstrategy3
+
+biomass1 = float(sys.argv[1])
+biomass2 = float(sys.argv[2])
+gly1= float(sys.argv[3]) ## gly a gly1; int() a float()
+gly2 = float(sys.argv[4]) ## tpha a a gly2; int() a float()
+o2 = float(sys.argv[5])
+fitness = sys.argv[6]
+
+plasticDegradationSimulationEstrategy3.Flycop(gly2,biomass1,biomass2,gly1,o2,fitness,'./IndividualRunsResults/',2)
